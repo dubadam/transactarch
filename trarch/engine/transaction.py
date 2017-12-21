400a9ccf874b36f06e1c7d67ee9202409ae63d16
Slava <phdslava@gmail.com>
2017-12-21 16:53:08 +0300

    Bit more transactions

--- trarch/engine/transaction.py
+++ trarch/engine/transaction.py
@@ -9,8 +9,20 @@ class Transaction:
         self.user = user_id
         self.date = datetime.now()
         self.cursor = cursor
+        self._id = False
+
+    @property
+    def status(self):
+        return isinstance(self._id, int)
 
     def start(self):
         query = "INSERT INTO " + self.tablename + "(user_id, date) VALUES(%s, %s) RETURNING id;"
         self.cursor.execute(query, (self.user, self.date))
         self._id = self.cursor.fetchone()[0]
+
+    def reset(self):
+        del self._id
+
+    @property
+    def id(self):
+        return self._id
