e701704ab550ba1f654cf567983821197bb8924b
Slava <phdslava@gmail.com>
2017-12-22 14:53:02 +0300

    Bit more transactions

--- trarch/engine/dbabstr.py
+++ trarch/engine/dbabstr.py
@@ -4,6 +4,4 @@ import psycopg2
 
 class Storage:
     def __init__(self):
-        self.conn = psycopg2.connect("dbname=test user=postgres")
-        self.cursor = self.conn.cursor()
-        self.t = Transaction(1, self.cursor)
+        self.t = Transaction(1)
--- trarch/engine/transaction.py
+++ trarch/engine/transaction.py
@@ -1,19 +1,24 @@
 from datetime import datetime
-
+import psycopg2
 
 class Transaction:
 
     tablename = 'transaction'
 
-    def __init__(self, user_id, cursor):
+    def __init__(self, user_id):
         self.user = user_id
         self.date = datetime.now()
-        self.cursor = cursor
+        self.conn = psycopg2.connect("dbname=test user=postgres")
+        self.cursor = self.conn.cursor()
         self._id = False
 
     @property
     def status(self):
         return isinstance(self._id, int)
+
+    @property
+    def id(self):
+        return self._id
 
     def start(self):
         query = "INSERT INTO " + self.tablename + "(user_id, date) VALUES(%s, %s) RETURNING id;"
@@ -21,8 +26,13 @@ class Transaction:
         self._id = self.cursor.fetchone()[0]
 
     def reset(self):
+        self.conn.rollback()
         del self._id
 
-    @property
-    def id(self):
-        return self._id
+    def commit(self):
+        self.conn.commit()
+        self.start()
+
+    def process_query(self, query, data):
+        self.cursor.execute(query, data)
+        return self.cursor.fetchall()
