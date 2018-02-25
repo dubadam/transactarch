5bf232e5f69c8bc51597ba3d98fd1124cc3eaba0
Slava <phdslava@gmail.com>
2018-02-25 18:21:21 +0300

    base repo code

---
+++ trarch/engine/repository.py
@@ -0,0 +1,10 @@
+class Repository:
+
+    tablename = 'repo'
+
+    def __init__(self, storage):
+        self._storage = storage
+        self._valid = False
+        self._id = False
+        self._guid = False
+        
